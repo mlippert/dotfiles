4107f2d811bb66dfd1deaa7d6677bf730e8fc305
Mike Lippert <mlippert255@gmail.com>
2015-08-23 12:52:28 -0400

    fix hookup_userdata_links.sh so it does what create-userdata-links.sh does, only cleaner.

--- bin/hookup_userdata_links.sh
+++ bin/hookup_userdata_links.sh
@@ -2,42 +2,78 @@
 # Move original files to .orig_files folder
 # create links to those files/folders in userdata
 
-USERDATADIR=/userdata/mjl/
+USER=mjl
+USERDATADIR=/userdata/${USER}/
+USERDOTFILESDIR=${USERDATADIR}dotfiles/
 
-declare -a USERFILES=(\
-		.bash_aliases\
-		.bashrc\
-		.config/bcompare\
-		.config/chromium\
-		.config/keepassx\
-		.config/libreoffice\
-		.config/quassel-irc.org\
-		.config/ReText\\ project\
-		.dia\
-		.gitconfig\
-		.gvimrc\
-		.kde/share/apps/ktorrent\
-		.kde/share/config/ktorrentrc\
-		.vimrc\
-		.inputrc\
-		.mozilla\
-		.purple\
-		.smartgit\
-		.ssh\
-		.vim\
-		bin\
-		Documents\
-		Downloads\
-		Music\
-		Pictures\
-		Projects\
-		Videos\
+# link to the directories in userdata
+declare -A USERDIRS=(\
+		[bin]=${USERDATADIR}bin\
+		[Documents]=${USERDATADIR}Documents\
+		[Downloads]=${USERDATADIR}Downloads\
+		[Music]=${USERDATADIR}Music\
+		[Pictures]=${USERDATADIR}Pictures\
+		[Projects]=${USERDATADIR}Projects\
+		[Videos]=${USERDATADIR}Videos\
 	)
 
-mkdir .orig_files
+# link to the configuration directories in userdata that are not in dotfiles (too large or too sensitive for github)
+declare -A USERCONFIGFILES=(\
+		[.config/Atlassian]=${USERDATADIR}.config/Atlassian\
+		[.config/bcompare]=${USERDATADIR}.config/bcompare\
+		[.config/chromium]=${USERDATADIR}.config/chromium\
+		[.config/keepassx]=${USERDATADIR}.config/keepassx\
+		[.config/libreoffice]=${USERDATADIR}.config/libreoffice\
+		[.config/ReText\ project]=${USERDATADIR}.config/ReText\ project\
+		[.dia]=${USERDATADIR}.dia\
+		[.mozilla]=${USERDATADIR}.mozilla\
+		[.purple]=${USERDATADIR}.purple\
+		[.smartgit]=${USERDATADIR}.smartgit\
+		[.ssh]=${USERDATADIR}.ssh\
+	)
 
-for t in "${USERFILES[@]}"
+# link to the configuration files and directories in the dotfiles repository
+declare -A USERDOTFILES=(\
+		[.bash_aliases]=${USERDOTFILESDIR}bash_aliases\
+		[.bashrc]=${USERDOTFILESDIR}bashrc
+		[.ctags]=${USERDOTFILESDIR}ctags
+		[.gitconfig]=${USERDOTFILESDIR}gitconfig
+		[.gvimrc]=${USERDOTFILESDIR}gvimrc
+		[.hgrc]=${USERDOTFILESDIR}hgrc
+		[.inputrc]=${USERDOTFILESDIR}inputrc
+		[.profile]=${USERDOTFILESDIR}profile
+		[.vim]=${USERDOTFILESDIR}vim
+		[.vimrc]=${USERDOTFILESDIR}vimrc
+		[.dir_colors]=${USERDOTFILESDIR}dircolors.ansi-dark
+		[.kde/share/apps/konsole/Solarized\ Dark.colorscheme]=${USERDOTFILESDIR}kde/share/apps/konsole/Solarized\ Dark.colorscheme\
+		[.kde/share/apps/konsole/Solarized\ Light.colorscheme]=${USERDOTFILESDIR}kde/share/apps/konsole/Solarized\ Light.colorscheme\
+		[.kde/share/apps/konsole/Solarized\ MJL.colorscheme]=${USERDOTFILESDIR}kde/share/apps/konsole/Solarized\ MJL.colorscheme\
+	)
+
+pushd ~
+
+mkdir -p userdata_backup/config
+echo Moving existing files to backup location...
+mv --verbose --backup=numbered --t userdata_backup/ ${!USERDIRS[@]}
+mv --verbose --backup=numbered --t userdata_backup/config ${!USERCONFIGFILES[@]}
+mv --verbose --backup=numbered --t userdata_backup/ ${!USERDOTFILES[@]}
+
+for f in "${!USERDIRS[@]}"
 do
-    mv $t .orig_files/
-    ln -s ${USERDATADIR}$t $t
+    echo linking $f to ${USERDIRS[$f]}...
+    ln -s "${USERDIRS[$f]}" "$f"
 done
+
+for f in "${!USERCONFIGFILES[@]}"
+do
+    echo linking $f to ${USERCONFIGFILES[$f]}...
+    ln -s "${USERCONFIGFILES[$f]}" "$f"
+done
+
+for f in "${!USERDOTFILES[@]}"
+do
+    echo linking $f to ${USERDOTFILES[$f]}...
+    ln -s "${USERDOTFILES[$f]}" "$f"
+done
+
+popd

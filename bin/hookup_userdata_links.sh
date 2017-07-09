20c75e8810414d55f9602b17ea17d39ce99ae85e
Mike Lippert <mlippert255@gmail.com>
2017-07-09 18:54:11 -0400

    enhanced hookup_userdata_links w/ cmdline options, removed create-userdata-links

--- bin/hookup_userdata_links.sh
+++ bin/hookup_userdata_links.sh
@@ -2,9 +2,79 @@
 # Move original files to .orig_files folder
 # create links to those files/folders in userdata
 
+# Argument = -u user -d dotfiles_path -l all|dotfiles|userdata
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This backs up and then links to files in dotfiles and files in userdata
+e.g.
+$0 -u mjl -d /home/mjl/dotfiles/ -l dotfiles
+
+OPTIONS:
+   -h   Show this message
+   -l   Files to link, can be 'all', 'dotfiles' or 'userdata' defaults to all
+   -u   user name (links are created in /home/{user}, and userdata path is /userdata/{user}
+        default user is mjl
+   -d   Dotfiles path. defaults to {userdata_path}/dotfiles/
+EOF
+}
+
 USER=mjl
+DOTFILESDIR=
+LINKDOTFILES=true
+LINKUSERDATAFILES=true
+
+
+while getopts “hl:u:d:” OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 1
+            ;;
+        u)
+            USER=$OPTARG
+            ;;
+        d)
+            DOTFILESDIR=$OPTARG
+            ;;
+        l)
+            case $OPTARG in
+                all)
+                    LINKDOTFILES=true
+                    LINKUSERDATAFILES=true
+                    ;;
+                dotfiles)
+                    LINKDOTFILES=true
+                    LINKUSERDATAFILES=false
+                    ;;
+                userdata)
+                    LINKDOTFILES=false
+                    LINKUSERDATAFILES=true
+                    ;;
+                *)
+                    usage
+                    exit 1
+                    ;;
+            esac
+            ;;
+        ?)
+            usage
+            exit
+            ;;
+    esac
+done
+
 USERDATADIR=/userdata/${USER}/
-USERDOTFILESDIR=${USERDATADIR}dotfiles/
+
+# if unspecified on the command line, set the Dotfiles dir to the default in the /userdata dir
+if [ "$DOTFILESDIR" = "" ]; then
+    DOTFILESDIR=${USERDATADIR}dotfiles/
+fi
+
 
 # link to the directories in userdata
 declare -A USERDIRS=(\
@@ -36,46 +106,56 @@ declare -A USERCONFIGFILES=(\
 
 # link to the configuration files and directories in the dotfiles repository
 declare -A USERDOTFILES=(\
-		[.bash_aliases]=${USERDOTFILESDIR}bash_aliases\
-		[.bashrc]=${USERDOTFILESDIR}bashrc
-		[.ctags]=${USERDOTFILESDIR}ctags
-		[.gitconfig]=${USERDOTFILESDIR}gitconfig
-		[.gvimrc]=${USERDOTFILESDIR}gvimrc
-		[.hgrc]=${USERDOTFILESDIR}hgrc
-		[.inputrc]=${USERDOTFILESDIR}inputrc
-		[.profile]=${USERDOTFILESDIR}profile
-		[.vim]=${USERDOTFILESDIR}vim
-		[.vimrc]=${USERDOTFILESDIR}vimrc
-		[.dir_colors]=${USERDOTFILESDIR}dircolors.ansi-dark
-		[.local/share/konsole/Solarized\ Dark.colorscheme]=${USERDOTFILESDIR}local/share/konsole/Solarized\ Dark.colorscheme\
-		[.local/share/konsole/Solarized\ Light.colorscheme]=${USERDOTFILESDIR}local/share/konsole/Solarized\ Light.colorscheme\
-		[.local/share/konsole/Solarized\ MJL.colorscheme]=${USERDOTFILESDIR}local/share/konsole/Solarized\ MJL.colorscheme\
+		[.bash_aliases]=${DOTFILESDIR}bash_aliases\
+		[.bashrc]=${DOTFILESDIR}bashrc\
+		[.ctags]=${DOTFILESDIR}ctags\
+		[.gitconfig]=${DOTFILESDIR}gitconfig\
+		[.gvimrc]=${DOTFILESDIR}gvimrc\
+		[.hgrc]=${DOTFILESDIR}hgrc\
+		[.inputrc]=${DOTFILESDIR}inputrc\
+		[.profile]=${DOTFILESDIR}profile\
+		[.vim]=${DOTFILESDIR}vim\
+		[.vimrc]=${DOTFILESDIR}vimrc\
+		[.dir_colors]=${DOTFILESDIR}dircolors.ansi-dark\
+		[.local/share/konsole/Solarized\ Dark.colorscheme]=${DOTFILESDIR}local/share/konsole/Solarized\ Dark.colorscheme\
+		[.local/share/konsole/Solarized\ Light.colorscheme]=${DOTFILESDIR}local/share/konsole/Solarized\ Light.colorscheme\
+		[.local/share/konsole/Solarized\ MJL.colorscheme]=${DOTFILESDIR}local/share/konsole/Solarized\ MJL.colorscheme\
 	)
 
-pushd ~
+pushd /home/${USER}
 
 mkdir -p userdata_backup/config
 echo Moving existing files to backup location...
-mv --verbose --backup=numbered --t userdata_backup/ ${!USERDIRS[@]}
-mv --verbose --backup=numbered --t userdata_backup/config ${!USERCONFIGFILES[@]}
-mv --verbose --backup=numbered --t userdata_backup/ ${!USERDOTFILES[@]}
 
-for f in "${!USERDIRS[@]}"
-do
-    echo linking $f to ${USERDIRS[$f]}...
-    ln -s "${USERDIRS[$f]}" "$f"
-done
+if [ $LINKUSERDATAFILES = true ]; then
+    mv --verbose --backup=numbered --t userdata_backup/ ${!USERDIRS[@]}
+    mv --verbose --backup=numbered --t userdata_backup/config ${!USERCONFIGFILES[@]}
+fi
 
-for f in "${!USERCONFIGFILES[@]}"
-do
-    echo linking $f to ${USERCONFIGFILES[$f]}...
-    ln -s "${USERCONFIGFILES[$f]}" "$f"
-done
+if [ $LINKDOTFILES = true ]; then
+    mv --verbose --backup=numbered --t userdata_backup/ ${!USERDOTFILES[@]}
+fi
 
-for f in "${!USERDOTFILES[@]}"
-do
-    echo linking $f to ${USERDOTFILES[$f]}...
-    ln -s "${USERDOTFILES[$f]}" "$f"
-done
+if [ $LINKUSERDATAFILES = true ]; then
+    for f in "${!USERDIRS[@]}"
+    do
+        echo linking $f to ${USERDIRS[$f]}...
+        ln -s "${USERDIRS[$f]}" "$f"
+    done
+
+    for f in "${!USERCONFIGFILES[@]}"
+    do
+        echo linking $f to ${USERCONFIGFILES[$f]}...
+        ln -s "${USERCONFIGFILES[$f]}" "$f"
+    done
+fi
+
+if [ $LINKDOTFILES = true ]; then
+    for f in "${!USERDOTFILES[@]}"
+    do
+        echo linking $f to ${USERDOTFILES[$f]}...
+        ln -s "${USERDOTFILES[$f]}" "$f"
+    done
+fi
 
 popd

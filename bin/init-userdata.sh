c99da49e4a3e24cd979dad10320f6e9e82be7eb3
Mike Lippert <mlippert255@gmail.com>
2023-01-31 14:02:54 -0500

    feat(bin): add some new .config directories to link; add a preliminary script to init a clean userdata dir
    The init-userdata script needs work, but it will help because I believe it will ensure that the targets of links in hookup_userdata_links exist.

---
+++ bin/init-userdata.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Create directories in userdata/<user> that hookup_userdata_links.sh will create symlinks to
+
+USER=mjl
+
+if [ -n "$1" ]; then
+    USER=$1
+fi
+
+if ! pushd /userdata/${USER}; then
+    exit 1
+fi
+
+# TODO: We really need to create all of these as the user $USER so they have the correct permissions
+
+mkdir .aws .beyondcompare .dia .docker .gnupg .gramps .minecraft .mozilla .purple .smartgit .ssh .wine
+mkdir Documents Downloads Music Pictures Videos bin
+touch .netrc .npmrc
+chmod go= .gnupg
+
+mkdir .config
+pushd .config
+mkdir Atlassian 'ReText project' Signal Slack bcompare calibre chromium discord keepassxc libreoffice smartgit teamviewer ukuu
+touch KeePassXCrc
+popd
+
+mkdir -p .local/share
+pushd .local/share
+mkdir icons wallpapers
+popd
+
+popd
